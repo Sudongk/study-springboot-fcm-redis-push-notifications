72ececd08b26d40d0935227ba1e18a46a377ed72
Sudongk <82927161+Sudongk@users.noreply.github.com>
2023-02-09 17:17:56 +0900

    test용 active profiles, board querydsl repository 단위 테스트

--- src/test/java/com/myboard/repository/RepositoryExTest.java
+++ src/test/java/com/myboard/repository/RepositoryExTest.java
@@ -11,14 +11,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.jdbc.Sql;
 
 import java.util.Arrays;
 
 @DataJpaTest(showSql = false)
 @Import(TestQuerydslConfig.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Sql(value = "classpath:/init.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 public class RepositoryExTest{
 
     @Autowired
@@ -40,13 +38,18 @@ public class RepositoryExTest{
 
     @BeforeEach
     void setUp() {
+        initUser();
+        initBoard();
+        initArticle();
+        initArticleComment();
+    }
+
+    private void initUser() {
         this.user1 = User.builder()
                 .username("test user1")
                 .password("password")
                 .role(User.Role.USER)
                 .build();
-
-//        this.user1.setId(1L);
 
         this.user2 = User.builder()
                 .username("test user2")
@@ -54,53 +57,57 @@ public class RepositoryExTest{
                 .role(User.Role.USER)
                 .build();
 
-//        this.user2.setId(2L);
+        userRepository.save(user1);
+        userRepository.save(user2);
+    }
 
+    private void initBoard() {
         this.board = Board.builder()
                 .boardName("boardName")
                 .user(this.user1)
                 .build();
 
-        this.board.addTags(Tag.convertListToTags(Arrays.asList("tag1, tag2")));
-//
-//        this.article1 = Article.builder()
-//                .title("title1")
-//                .content("content1")
-//                .viewCount(0L)
-//                .user(this.user1)
-//                .board(this.board)
-//                .build();
-//
-//        this.article2 = Article.builder()
-//                .title("title2")
-//                .content("content2")
-//                .viewCount(0L)
-//                .user(this.user1)
-//                .board(this.board)
-//                .build();
-//
-//        this.articleComment1 = ArticleComment.builder()
-//                .comment("comment1")
-//                .user(this.user2)
-//                .article(this.article1)
-//                .build();
-//
-//        this.articleComment2 = ArticleComment.builder()
-//                .comment("comment2")
-//                .user(this.user2)
-//                .article(this.article2)
-//                .build();
-
-        userRepository.save(user1);
-        userRepository.save(user2);
+        this.board.addTags(Tag.convertListToTags(Arrays.asList("tag1", "tag2")));
 
         boardRepository.save(board);
-//
-//        articleRepository.save(article1);
-//        articleRepository.save(article2);
-//
-//        articleCommentRepository.save(articleComment1);
-//        articleCommentRepository.save(articleComment2);
+    }
+
+    private void initArticle() {
+        this.article1 = Article.builder()
+                .title("title1")
+                .content("content1")
+                .viewCount(0L)
+                .user(this.user1)
+                .board(this.board)
+                .build();
+
+        this.article2 = Article.builder()
+                .title("title2")
+                .content("content2")
+                .viewCount(0L)
+                .user(this.user1)
+                .board(this.board)
+                .build();
+
+        articleRepository.save(article1);
+        articleRepository.save(article2);
+    }
+
+    private void initArticleComment() {
+        this.articleComment1 = ArticleComment.builder()
+                .comment("comment1")
+                .user(this.user2)
+                .article(this.article1)
+                .build();
+
+        this.articleComment2 = ArticleComment.builder()
+                .comment("comment2")
+                .user(this.user2)
+                .article(this.article2)
+                .build();
+
+        articleCommentRepository.save(articleComment1);
+        articleCommentRepository.save(articleComment2);
     }
 }
 
