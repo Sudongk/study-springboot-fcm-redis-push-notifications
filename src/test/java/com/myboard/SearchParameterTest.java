a7b82730428ebb8d7ed4c5bc9680e7da8e853fc0
Sudongk <82927161+Sudongk@users.noreply.github.com>
2023-02-18 23:53:27 +0900

    SearchParameter 생성 테스트

---
+++ src/test/java/com/myboard/SearchParameterTest.java
@@ -0,0 +1,53 @@
+package com.myboard;
+
+import com.myboard.dto.requestDto.search.SearchParameter;
+
+import com.myboard.exception.search.InvalidPageSizeException;
+import com.myboard.exception.search.InvalidPageStartException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SearchParameterTest {
+
+
+    @Test
+    @DisplayName("검색 requestDto인 SearchParameter 생성 - 성공")
+    void createSearchParameterSuccessful() {
+        // when, then
+        assertThatCode(() -> SearchParameter.of(
+                        "keyword",
+                        "board",
+                        "0",
+                        "20"
+                )
+        ).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("검색 requestDto인 SearchParameter 생성 - 실패 - start 값이 음수인 경우")
+    void createSearchParameterWithInValidStart() {
+        // when, then
+        assertThatThrownBy(() -> SearchParameter.of(
+                        "keyword",
+                        "board",
+                        "-1",
+                        "20"
+                )
+        ).isInstanceOf(InvalidPageStartException.class);
+    }
+
+    @Test
+    @DisplayName("검색 requestDto인 SearchParameter 생성 - 실패 - 유효하지 않은 size 값인 경우")
+    void createSearchParameterWithInValidSize() {
+        // when, then
+        assertThatThrownBy(() -> SearchParameter.of(
+                        "keyword",
+                        "board",
+                        "0",
+                        "0"
+                )
+        ).isInstanceOf(InvalidPageSizeException.class);
+    }
+}
