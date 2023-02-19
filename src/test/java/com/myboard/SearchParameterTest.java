9955594f78ccd52680a1e5d36a8321bc6c6f3e5d
Sudongk <82927161+Sudongk@users.noreply.github.com>
2023-02-19 14:38:10 +0900

    SearchKeyword 객체 생성 테스트

--- src/test/java/com/myboard/SearchParameterTest.java
+++ src/test/java/com/myboard/SearchParameterTest.java
@@ -13,7 +13,7 @@ public class SearchParameterTest {
 
 
     @Test
-    @DisplayName("검색 requestDto인 SearchParameter 생성 - 성공")
+    @DisplayName("검색 요청 SearchParameter 인스턴스 생성 성공")
     void createSearchParameterSuccessful() {
         // when, then
         assertThatCode(() -> SearchParameter.of(
@@ -26,7 +26,7 @@ public class SearchParameterTest {
     }
 
     @Test
-    @DisplayName("검색 requestDto인 SearchParameter 생성 - 실패 - start 값이 음수인 경우")
+    @DisplayName("검색 요청 SearchParameter 인스턴스 생성 실패 - start 값이 음수인 경우")
     void createSearchParameterWithInValidStart() {
         // when, then
         assertThatThrownBy(() -> SearchParameter.of(
@@ -39,7 +39,7 @@ public class SearchParameterTest {
     }
 
     @Test
-    @DisplayName("검색 requestDto인 SearchParameter 생성 - 실패 - 유효하지 않은 size 값인 경우")
+    @DisplayName("검색 요청 SearchParameter 인스턴스 생성 실패 - 유효하지 않은 size 값인 경우")
     void createSearchParameterWithInValidSize() {
         // when, then
         assertThatThrownBy(() -> SearchParameter.of(
